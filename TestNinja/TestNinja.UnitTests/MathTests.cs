d87c85d427cb8c510098ea2a5fc8027ca6708569
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-16 15:51:37 +0700

    2_7Black_box_Testing

--- TestNinja/TestNinja.UnitTests/MathTests.cs
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -10,9 +10,40 @@ namespace TestNinja.UnitTests
         public void Add_Whencalled_ReturnTheSumOfArguments()
         {
             var math = new Math();
+            
             var result = math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+       
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1,2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
         }
     }
 }
