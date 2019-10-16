5096f27c06062a8bdea53928bc1e1f7295e15b9b
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-16 15:12:17 +0700

    1_9Refactoring_with_Confidence

--- TestNinja/TestNinja/fundamentals/Reservation.cs
+++ TestNinja/TestNinja/fundamentals/Reservation.cs
@@ -3,19 +3,11 @@
     public class Reservation
     {
         public User MadeBy { get; set; }
-
         public bool CanBeCancelledBy(User user)
         {
-            if (user.IsAdmin)
-                return false;
-            if (MadeBy == user)
-                return false;
-
-            return false;
+            return (user.IsAdmin || MadeBy == user);
         }
-
     }
-
     public class User
     {
         public bool IsAdmin { get; set; }
