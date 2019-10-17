5485964aa286b15b463f732b83ee759b9e801c47
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-17 22:36:22 +0700

    3_4Testing_the_Return_Type_of_Methods

---
+++ TestNinja/TestNinja/fundamentals/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿namespace TestNinja.Fundamentals
+{
+    public class CustomerController
+    {
+        public ActionResult GetCustomer(int id)
+        {
+            if (id == 0)
+                return new NotFound();
+
+            return new ok();
+        }
+    }
+
+    public class ActionResult { }
+    
+    public class NotFound : ActionResult { }
+
+    public class ok : ActionResult { }
+}
