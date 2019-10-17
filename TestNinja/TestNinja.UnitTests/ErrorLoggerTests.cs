91e44fb2b6a02f0ae67932146344c9f6c1f1f1ea
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-17 23:35:58 +0700

    3_8Testing_Private_Methods

--- TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -40,7 +40,6 @@ namespace TestNinja.UnitTests
 
             Assert.That(id, Is.Not.EqualTo(Guid.Empty));
 
-        }
-
+        }  
     }
 }
--- TestNinja/TestNinja/fundamentals/ErrorLogger.cs
+++ TestNinja/TestNinja/fundamentals/ErrorLogger.cs
@@ -8,18 +8,20 @@ namespace TestNinja.Fundamentals
         public string LastError { get; set; }
 
         public event EventHandler<Guid> ErrorLogged;
-
+        private Guid _errorId;
         public void Log(string error)
-        {         
+        {
             if (String.IsNullOrWhiteSpace(error))
-               throw new ArgumentNullException();
+                throw new ArgumentNullException();
 
             LastError = error;
 
             // Write the log to a storage
             // ...
 
-            ErrorLogged?.Invoke(this, Guid.NewGuid());
+            _errorId = Guid.NewGuid();
+            ErrorLogged?.Invoke(this,_errorId);
+        
         }
     }
 }
