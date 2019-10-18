3871a597bf12aa008e2c441259aba064b956dd6f
PrakhanRattakool <56579980+PrakhanRattakool@users.noreply.github.com>
2019-10-18 18:14:00 +0700

    5_5Dependency_Injection_via_Properties

--- TestNinja/TestNinja/Mocking/Program.cs
+++ TestNinja/TestNinja/Mocking/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNinja.Mocking
+﻿namespace TestNinja.Mocking
 {
     class Program
     {
         public static void Main()
         {
             var service = new VideoService();
-            var title = service.ReadVideoTitle(new FileReader);
+            var title = service.ReadVideoTitle();
         }
     }
 }
